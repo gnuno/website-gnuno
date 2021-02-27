10e314698ba0fc4f2f03685431ac2d99f92585d7
Matias <matiasnoriega86@gmail.com>
2021-02-26 22:11:21 -0300

    (feat): footer principal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="root">
     <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 <script>
 import { defineComponent } from "vue";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default defineComponent({
   name: "app",
-  components: { Header },
+  components: { Footer, Header },
   setup() {}
 });
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>GNUNO {{ year }}</footer>
+</template>
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Footer",
+  setup() {
+    const year = ref(new Date().getFullYear().toString());
+    return { year };
+  }
+});
+</script>
+<style>
+footer {
+  width: 100%;
+  font-size: 10px;
+  text-align: center;
+}
+</style>
