cdda6c4aafd780ee67953f7b612b98e92c4b0626
Matias <matiasnoriega86@gmail.com>
2021-02-28 05:07:44 -0300

    (feat): configuracion inicial de rutas para router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [{}];
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "Home" */ "../views/Home")
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
