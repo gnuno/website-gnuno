6412a60e96329fdc8dd1c76c3d93ff5309a5cf24
Matias <matiasnoriega86@gmail.com>
2021-02-28 00:08:33 -0300

    (feat): navbars dentro de barra lateral convertidas en componentes

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,64 +1,56 @@
 <template>
   <div class="barra_lateral">
-    <div class="navbar">
-      <h3>Secciones</h3>
-      <ul>
-        <li>Inicio</li>
-        <li>Software Libre</li>
-        <li>¿Qué es GNUno?</li>
-        <li>Artículos</li>
-        <li>Proyectos</li>
-        <li>Eventos</li>
-        <li>Chat</li>
-        <li>Contacto</li>
-      </ul>
-    </div>
-
-    <div class="navbar">
-      <h3>Links</h3>
-      <ul>
-        <li>Universidad Nacional del Oeste</li>
-        <li>ForoUNO</li>
-        <li>Proyecto GNU</li>
-        <li>Free Software Foundation</li>
-      </ul>
-    </div>
+    <ContenedorLinks
+      v-bind:key="navbar"
+      v-for="navbar in navbars"
+      :titulo="navbar.titulo"
+      :links="navbar.links"
+    ></ContenedorLinks>
   </div>
 </template>
 
-<script></script>
+<script>
+import { defineComponent } from "vue";
+import ContenedorLinks from "@/components/ContenedorLinks";
+
+export default defineComponent({
+  name: "BarraLateral",
+  components: { ContenedorLinks },
+  data() {
+    return {
+      navbars: [
+        {
+          titulo: "Secciones",
+          links: [
+            "Inicio",
+            "Software Libre",
+            "¿Que es GNUno?",
+            "Artículos",
+            "Proyectos",
+            "Eventos",
+            "Chat",
+            "Contacto"
+          ]
+        },
+        {
+          titulo: "Links",
+          links: [
+            "Universidad Nacional del Oeste",
+            "ForoUNO",
+            "Proyecto GNU",
+            "Free Software Foundation"
+          ]
+        }
+      ]
+    };
+  }
+});
+</script>
 <style>
 .barra_lateral {
   display: block;
   float: left;
   margin-left: 1%;
   width: 15%;
-}
-.navbar {
-  background: #d6e6f2;
-  border: 1px solid #457585;
-  border-radius: 15px;
-  box-shadow: 2px 2px 5px grey;
-  text-align: center;
-  margin-bottom: 10%;
-}
-.navbar ul {
-  list-style: none;
-  width: 100%;
-  text-align: center;
-  padding-inline-start: 0px;
-}
-
-.navbar ul li {
-  width: 90%;
-  margin: 15px auto;
-  border-radius: 7px;
-  border: 2px solid #506362;
-  background: #6c929f;
-  color: #104050;
-  box-shadow: 2px 2px 5px grey;
-}
-.navbar ul li a {
-  text-decoration: none;
 }
 </style>
---
+++ src/components/ContenedorLinks.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="navbar">
+    <h3>{{ titulo }}</h3>
+    <ul>
+      <li v-for="link in links" v-bind:key="link">{{ link }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ContenedorLinks",
+  props: {
+    titulo: String,
+    links: Array
+  }
+});
+</script>
+<style scoped>
+.navbar {
+  background: #d6e6f2;
+  border: 1px solid #457585;
+  border-radius: 15px;
+  box-shadow: 2px 2px 5px grey;
+  text-align: center;
+  margin-bottom: 10%;
+}
+.navbar ul {
+  list-style: none;
+  width: 100%;
+  text-align: center;
+  padding-inline-start: 0px;
+}
+
+.navbar ul li {
+  width: 90%;
+  margin: 15px auto;
+  border-radius: 7px;
+  border: 2px solid #506362;
+  background: #6c929f;
+  color: #104050;
+  box-shadow: 2px 2px 5px grey;
+}
+.navbar ul li a {
+  text-decoration: none;
+}
+</style>
