3645b9a7fdf8057083456d6c5db9a8ff0eca4682
Matias <matiasnoriega86@gmail.com>
2021-02-28 05:06:53 -0300

    (feat): mock de notas provisorio

---
+++ src/mock/notas.js
@@ -0,0 +1,32 @@
+const notas = [
+  {
+    titulo: "Bienvenidxs!",
+    cuerpo:
+      "Bienvenidos a la página de GNUno, grupo de usuarios de GNU/Linux de la Universidad Nacional del Oeste, en este portal vamos a ir subiendo las distintas novedades con respecto a noticias y eventos.\n",
+    fecha: "2018-09-02",
+    autor: "Matías Nicolás Noriega"
+  },
+  {
+    titulo: "FLISoL 2017 - Sede Merlo\n",
+    cuerpo:
+      "En estos dias entregamos una nota al Decano de nuestra universidad solicitando el espacio para llevar a cabo el FLISoL 2017 en las instalaciones de la Universidad Nacional del Oeste. Como estudiantes de la misma estamos muy emocionados por llevar adelante el evento, e invitamos a toda la comunidad a participar.\n" +
+      "\n" +
+      "Nota\n" +
+      "\n" +
+      "Para mas información seguinos en Facebook como GNUno o inscribite en la lista de correos.\n" +
+      "\n",
+    fecha: "2018-09-0",
+    autor: "Matías Nicolás Noriega"
+  },
+  {
+    titulo: "Instalar PostgreSQL y pgAdmin III en Linux\n",
+    cuerpo:
+      "Nuestro compañero David se tomó el tiempo para hacer un videotutorial de como instalar PostgreSQL y pgAdmin, necesarios para la cursada de Base de Datos I en la UNO. En palabras de él:\n" +
+      "Me tomé el trabajo de hacer un pequeño vídeotutorial para mostrarles como se hace para instalar esos programas en linux y así poder empezar a practicar. Ojalá les sirva. Tenganme paciencia, es mi primer screencast, de a poco voy a ir mejorando. Ah y por cierto, normalmente escribo más rápido en el teclado, solo que en esta oportunidad lo hice con una sola mano, porque en la otra tenía el celular, grabé el audio en el celular ya que me parecía que tenía mayor claridad. \n" +
+      "Les dejamos el video, saludos!\n",
+    fecha: "2018-11-13",
+    autor: "Matías Nicolás Noriega"
+  }
+];
+
+export default notas;
