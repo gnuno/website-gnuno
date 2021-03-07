1fd29120974a74e60da5813022a0529c609a5f44
Matias <matiasnoriega86@gmail.com>
2021-03-07 20:01:18 -0300

    (feat): CKEditor

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Ckeditor from "@ckeditor/ckeditor5-vue";
 
 createApp(App)
   .use(store)
+  .use(Ckeditor)
   .use(router)
   .mount("#app");
